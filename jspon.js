16378f9e05becf06fa0794352e5b4484b951b612
martin-rueegg <martin.rueegg@metaworx.ch>
2017-02-09 18:40:46 +0000

    add onWalk

--- jspon.js
+++ jspon.js
@@ -1,188 +1,310 @@
-JSPON = (function () {
-    'use strict';
-    var idFieldName = 'b5b813f0-9f2c-4fd5-893e-c1bc36f2aa48';
-    var useIdBase = false;
-    var preserveArrays = true;
-    var jsonParser = JSON.parse;
-    var jsonStringifier = JSON.stringify;
-    var jsonPathRoot = '$';
-    var useJSONPathDotNotation = true;
+"use strict";
 
-    function setSettings(newSettings) {
-        if (!newSettings) {
-            newSettings = {};
-        }
-        if (Object.prototype.hasOwnProperty.call(newSettings, 'preserveArrays')) {
-            preserveArrays = newSettings.preserveArrays;
-        } else {
-            preserveArrays = true;
-        }
-        if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonPathRoot')) {
-            jsonPathRoot = newSettings.jsonPathRoot;
-            useIdBase = false;
-            idFieldName = undefined;
-        } else {
-            jsonPathRoot = '$';
-            useIdBase = false;
-            idFieldName = undefined;
-        }
-        if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonPathFormat')) {
-            if (newSettings['jsonPathFormat'] === 'BracketNotation') {
-                useJSONPathDotNotation = false;
-            } else {
-                useJSONPathDotNotation = true;
-            }
-        } else {
-            useJSONPathDotNotation = true;
-        }
-        if (Object.prototype.hasOwnProperty.call(newSettings, 'idFieldName')) {
-            idFieldName = newSettings.idFieldName;
-            useIdBase = true;
-            jsonPathRoot = undefined;
-        } else {
-            idFieldName = 'b5b813f0-9f2c-4fd5-893e-c1bc36f2aa48';
-            useIdBase = false;
-        }
-        if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonParser')) {
-            jsonParser = newSettings.parse;
-        } else {
-            jsonParser = JSON.parse;
-        }
-        if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonStringifier')) {
-            jsonStringifier = newSettings.stringify
-        } else {
-            jsonStringifier = JSON.stringify;
-        }
-    }
-    function getSettings() {
-        return {
-            'idFieldName': idFieldName,
-            'useIdBase': useIdBase,
-            'preserveArrays': preserveArrays,
-            'jsonParser': jsonParser,
-            'jsonStringifier': jsonStringifier,
-            'jsonPathRoot': jsonPathRoot,
-            'jsonPathFormat': useJSONPathDotNotation ? 'DotNotation' : 'BracketNotation'
-        };
-    }
-    var isMapSupported = true;
-    try {
-        Map;
-    }
-    catch (e) {
-        isMapSupported = false;
-        console.log("Map is not supported.");
-    }
+var i = 0;
+var defaultInstance;
 
-    function jsponClone(objTracker, oldValue, id) {
-        if (typeof oldValue === 'object' && oldValue !== null &&
-				!(oldValue instanceof Boolean) &&
-				!(oldValue instanceof Date) &&
-				!(oldValue instanceof Number) &&
-				!(oldValue instanceof RegExp) &&
-				!(oldValue instanceof String)) {
-            if (isMapSupported) {
-                if (objTracker.has(oldValue)) {
-                    return { '$ref': objTracker.get(oldValue) };
-                }
-            } else {
-                if (Object.prototype.hasOwnProperty.call(oldValue, idFieldName)) {
-                    return { '$ref': oldValue[idFieldName] };
-                }
-            }
-            var newValue = oldValue.constructor();
-            var isArray = oldValue.constructor === Array;
-            if (isArray) {
-                var arraySectionObj = newValue;
-            }
-            if (useIdBase) {
-                if (preserveArrays && isArray) {
-                    newValue = { '$values': newValue };
-                }
-                if (!(preserveArrays === false && isArray)) {
-                    id.uniqueId++;
-                    if (isMapSupported) {
-                        objTracker.set(oldValue, id.uniqueId);
-                        newValue[idFieldName] = id.uniqueId;
-                    } else {
-                        oldValue[idFieldName] = id.uniqueId;
-                        objTracker[id.uniqueId] = oldValue;
-                        newValue[idFieldName] = id.uniqueId;
-                    }
-                }
-            } else {
-                if (!(preserveArrays === false && isArray)) {
-                    if (isMapSupported) {
-                        objTracker.set(oldValue, id);
-                    } else {
-                        oldValue[idFieldName] = id;
-                        objTracker[id] = oldValue;
-                        objTracker['new' + id] = newValue;
-                    }
-                }
-            }
+var JSPON = module.exports = function(config)
+{
 
-            if (isArray) {
-                for (var i = 0; i < oldValue.length; i++) {
-                    arraySectionObj.push(jsponClone(objTracker, oldValue[i], useIdBase ? id : id + '[' + i + ']'));
-                }
-            } else {
-                for (var key in oldValue) {
-                    if (Object.prototype.hasOwnProperty.call(oldValue, key)) {
-                        newValue[key] = jsponClone(objTracker, oldValue[key], useIdBase ? id : useJSONPathDotNotation ? id + '.' + key : id + '[\'' + key + '\']');
-                    }
-                }
-            }
-            return newValue;
-        }
-        return oldValue;
-    }
+	if (!(this instanceof JSPON))
+	{
+		return JSPON.defaultInstance(config);
+	}
+
+	this.idFieldName = 'b5b813f0-9f2c-4fd5-893e-c1bc36f2aa48';
+	this.useIdBase = false;
+	this.preserveArrays = true;
+	this.jsonParser = JSON.parse;
+	this.jsonStringifier = JSON.stringify;
+	this.jsonPathRoot = '$';
+	this.useJSONPathDotNotation = true;
+	this.i = ++i;
+	function onWalk(loc, expression, value, path) {};
+	this.onWalk = function(loc, expression, value, path) {};
+	this.onWalk = onWalk;
+	
+	this.setSettings(config);
+}
+
+JSPON.defaultInstance = function(config = {})
+{
+	if (!(defaultInstance instanceof JSPON))
+	{
+		defaultInstance = new JSPON(config);
+	}
+
+	return defaultInstance;
+}
+
+JSPON.setSettings = function(newSettings)
+{
+	JSPON.defaultInstance().setSettings(newSettings);
+}
+
+JSPON.prototype.setSettings = function(newSettings) {
+	if (!newSettings) {
+		newSettings = {};
+	}
+
+	if (Object.prototype.hasOwnProperty.call(newSettings, 'preserveArrays')) {
+		this.preserveArrays = newSettings.this.preserveArrays;
+	} else {
+		this.preserveArrays = true;
+	}
+
+	if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonPathRoot')) {
+		this.jsonPathRoot = newSettings.this.jsonPathRoot;
+		this.useIdBase = false;
+		this.idFieldName = undefined;
+	} else {
+		this.jsonPathRoot = '$';
+		this.useIdBase = false;
+		this.idFieldName = undefined;
+	}
+
+	if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonPathFormat')) {
+		if (newSettings['jsonPathFormat'] === 'BracketNotation') {
+			this.useJSONPathDotNotation = false;
+		} else {
+			this.useJSONPathDotNotation = true;
+		}
+	} else {
+		this.useJSONPathDotNotation = true;
+	}
+
+	if (Object.prototype.hasOwnProperty.call(newSettings, 'idFieldName')) {
+		this.idFieldName = newSettings.this.idFieldName;
+		this.useIdBase = true;
+		this.jsonPathRoot = undefined;
+	} else {
+		this.idFieldName = 'b5b813f0-9f2c-4fd5-893e-c1bc36f2aa48';
+		this.useIdBase = false;
+	}
+
+	if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonParser')) {
+		this.jsonParser = newSettings.parse;
+	} else {
+		this.jsonParser = JSON.parse;
+	}
+
+	if (Object.prototype.hasOwnProperty.call(newSettings, 'jsonStringifier')) {
+		this.jsonStringifier = newSettings.stringify
+	} else {
+		this.jsonStringifier = JSON.stringify;
+	}
+
+	if (Object.prototype.hasOwnProperty.call(newSettings, 'onWalk'))
+	{
+		if (newSettings.onWalk.constructor === Array)
+		{
+			this.onWalk = newSettings.onWalk;
+		}
+		else if (this.onWalk.constructor === Array)
+		{
+			this.onWalk.push(newSettings.onWalk);
+		}
+		else
+		{
+			this.onWalk = [newSettings.onWalk];
+		}
+	}
+}
+
+JSPON.setSettings = function(newSettings)
+{
+	return JSPON.defaultInstance().getSettings();
+}
+
+JSPON.prototype.getSettings = function() {
+	return {
+		'idFieldName': this.idFieldName,
+		'useIdBase': this.useIdBase,
+		'preserveArrays': this.preserveArrays,
+		'jsonParser': this.jsonParser,
+		'jsonStringifier': this.jsonStringifier,
+		'jsonPathRoot': this.jsonPathRoot,
+		'jsonPathFormat': this.useJSONPathDotNotation ? 'DotNotation' : 'BracketNotation'
+	};
+}
+
+JSPON.isMapSupported = true;
+try {
+	Map;
+}
+catch (e) {
+	JSPON.isMapSupported = false;
+	console.log("Map is not supported.");
+}
+
+JSPON.jsponClone = function(objTracker, oldValue, id)
+{
+	return JSPON.defaultInstance().jsponClone(objTracker, oldValue, id);
+}
+
+JSPON.prototype.jsponClone = function(objTracker, oldValue, id)
+{
+	if (typeof oldValue === 'object' && oldValue !== null &&
+			!(oldValue instanceof Boolean) &&
+			!(oldValue instanceof Date) &&
+			!(oldValue instanceof Number) &&
+			!(oldValue instanceof RegExp) &&
+			!(oldValue instanceof String)) {
+		if (this.isMapSupported) {
+			if (objTracker.has(oldValue)) {
+				return { '$ref': objTracker.get(oldValue) };
+			}
+		} else {
+			if (Object.prototype.hasOwnProperty.call(oldValue, this.idFieldName)) {
+				return { '$ref': oldValue[this.idFieldName] };
+			}
+		}
+		var newValue = oldValue.constructor();
+		var isArray = oldValue.constructor === Array;
+		if (isArray) {
+			var arraySectionObj = newValue;
+		}
+		if (this.useIdBase) {
+			if (this.preserveArrays && isArray) {
+				newValue = { '$values': newValue };
+			}
+			if (!(this.preserveArrays === false && isArray)) {
+				id.uniqueId++;
+				if (this.isMapSupported) {
+					objTracker.set(oldValue, id.uniqueId);
+					newValue[this.idFieldName] = id.uniqueId;
+				} else {
+					oldValue[this.idFieldName] = id.uniqueId;
+					objTracker[id.uniqueId] = oldValue;
+					newValue[this.idFieldName] = id.uniqueId;
+				}
+			}
+		} else {
+			if (!(this.preserveArrays === false && isArray)) {
+				if (this.isMapSupported) {
+					objTracker.set(oldValue, id);
+				} else {
+					oldValue[this.idFieldName] = id;
+					objTracker[id] = oldValue;
+					objTracker['new' + id] = newValue;
+				}
+			}
+		}
+
+		if (isArray) {
+			for (var i = 0; i < oldValue.length; i++) {
+				arraySectionObj.push(this.jsponClone(objTracker, oldValue[i], this.useIdBase ? id : id + '[' + i + ']'));
+			}
+		} else {
+			for (var key in oldValue) {
+				if (Object.prototype.hasOwnProperty.call(oldValue, key)) {
+					newValue[key] = this.jsponClone(objTracker, oldValue[key], this.useIdBase ? id : this.useJSONPathDotNotation ? id + '.' + key : id + '[\'' + key + '\']');
+				}
+			}
+		}
+		return newValue;
+	}
+	return oldValue;
+}
 
 
 
-    function jsponParse(objTracker, obj, id) {
-        if (obj && typeof obj === 'object') {
-            if (Object.prototype.hasOwnProperty.call(obj, '$ref')) {
-                return objTracker[obj['$ref']];
-            }
-            if (Object.prototype.hasOwnProperty.call(obj, '$values') && preserveArrays && useIdBase) {
-                obj = obj['$values'];
-            }
-            objTracker[id] = obj;
-            if (obj.constructor === Array) {
-                for (var i = 0; i < obj.length; i++) {
-                    obj[i] = jsponParse(objTracker, obj[i], useIdBase ? obj[i][idFieldName] : id + '[' + i + ']');
-                }
-            } else {
-                for (var key in obj) {
-                    obj[key] = jsponParse(objTracker, obj[key], useIdBase ? obj[key][idFieldName] : useJSONPathDotNotation ? id + '.' + key : id + '[\'' + key + '\']');
-                }
-            }
-            if (useIdBase) delete obj[idFieldName];
-        }
-        return obj;
-    }
+JSPON.jsponParse = function(objTracker, oldValue, id)
+{
+	return JSPON.defaultInstance().jsponParse(objTracker, oldValue, id);
+}
 
-    function stringify(obj) {
-        var objTracker = isMapSupported ? new Map() : {};
-        var newObj = jsponClone(objTracker, obj, useIdBase ? { uniqueId: 0 } : jsonPathRoot);
-        if (!isMapSupported) {
-            for (var x in objTracker) {
-                delete objTracker[x][idFieldName];
-            }
-        }
-        return jsonStringifier(newObj);
-    }
+JSPON.prototype.jsponParse = function(objTracker, obj, id)
+{
+	if (this.onWalk !== undefined)
+	{
+		if (!Array.isArray(this.onWalk))
+		{
+			this.onWalk = [this.onWalk];
+		}
 
-    function parse(str) {
-        var obj = jsonParser(str);
-        return jsponParse({}, obj, useIdBase ? obj[idFieldName] : jsonPathRoot);
-    }
+		for (var i = 0; i < this.onWalk.length; i++)
+		{
+			if (this.onWalk[i].constructor === Function)
+			{
+				obj = this.onWalk[i](id, obj) || obj;
+			}
+		}
+	}
 
-    return {
-        parse: parse,
-        stringify: stringify,
-        setSettings: setSettings,
-        getSettings: getSettings
-    };
-})();
+	if (obj && typeof obj === 'object') {
+		if (Object.prototype.hasOwnProperty.call(obj, '$ref')) {
+			return objTracker[obj['$ref']];
+		}
+		if (Object.prototype.hasOwnProperty.call(obj, '$values') && this.preserveArrays && this.useIdBase) {
+			obj = obj['$values'];
+		}
+		objTracker[id] = obj;
+		if (obj.constructor === Array) {
+			var tempObj = {};
+			var allWithId = true;
+
+			for (var i = 0; i < obj.length; i++) {
+				obj[i] = this.jsponParse(objTracker, obj[i], this.useIdBase ? obj[i][this.idFieldName] : id + '[' + i + ']');
+				if (obj[i]
+				&& typeof obj[i] === 'object'
+					&& Object.prototype.hasOwnProperty.call(obj[i], 'id'))
+				{
+					objTracker[id+'.'+obj[i].id] = obj[i];
+					if (allWithId)
+					{
+						tempObj[obj[i].id] = obj[i];
+					}
+				}
+				else
+				{
+					allWithId = false;
+				}
+			}
+
+			if (allWithId)
+			{
+				obj = tempObj;
+			}
+		} else {
+			for (var key in obj) {
+				obj[key] = this.jsponParse(objTracker, obj[key], this.useIdBase ? obj[key][this.idFieldName] : this.useJSONPathDotNotation ? id + '.' + key : id + '[\'' + key + '\']');
+			}
+		}
+		if (this.useIdBase) delete obj[this.idFieldName];
+	}
+	return obj;
+}
+
+JSPON.stringify = function(obj)
+{
+	return JSPON.defaultInstance().stringify(obj);
+}
+
+JSPON.prototype.stringify = function(obj) {
+	var objTracker = this.isMapSupported ? new Map() : {};
+	var newObj = this.jsponClone(objTracker, obj, this.useIdBase ? { uniqueId: 0 } : this.jsonPathRoot);
+	if (!this.isMapSupported) {
+		for (var x in objTracker) {
+			delete objTracker[x][this.idFieldName];
+		}
+	}
+	return this.jsonStringifier(newObj);
+}
+
+JSPON.parse = function(str)
+{
+	return JSPON.defaultInstance().parse(str);
+}
+
+JSPON.prototype.parse = function(str) {
+	var obj = this.jsonParser(str);
+	return this.jsponParse({}, obj, this.useIdBase ? obj[this.idFieldName] : this.jsonPathRoot);
+}
+
+//return {
+//parse: parse,
+//stringify: stringify,
+//setSettings: setSettings,
+//getSettings: getSettings
+//};
+//})();
